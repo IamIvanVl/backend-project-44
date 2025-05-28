3fe77d0cefe9cd0ab688e4165a0e3021db62819a
Иван Власенко <gimo135@yandex.ru>
2025-05-28 16:53:41 +0300

    Add brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import execute from '../src/engine.js'
+import gamePrime from '../src/games/prime.js'
+
+execute(gamePrime)
---
+++ src/games/prime.js
@@ -0,0 +1,20 @@
+import getRandomInRange from '../randomizer.js'
+import isPrime from '../isPrime.js'
+
+const game = () => {
+  const question = getRandomInRange(1, 100)
+  const isQuestionPrime = isPrime(question)
+
+  let correctAnswer
+
+  if (isQuestionPrime === 'true') {
+    correctAnswer = 'yes'
+  }
+  else {
+    correctAnswer = 'no'
+  }
+
+  return [question, correctAnswer]
+}
+
+export default game
