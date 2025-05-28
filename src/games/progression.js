e1656fb538ca1aea18318c46c6af54a0485ae62a
Иван Власенко <gimo135@yandex.ru>
2025-05-28 18:03:11 +0300

    Update README.md and fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,7 +4,8 @@ import getRandomInRange from '../randomizer.js'
 const game = () => {
   const start = getRandomInRange(1, 30)
   const step = getRandomInRange(1, 10)
-  const sequence = createSequence(start, step, 10)
+  const length = getRandomInRange(5, 10)
+  const sequence = createSequence(start, step, length)
   const blankIndex = getRandomInRange(0, sequence.length - 1)
   const correctAnswer = String(sequence[blankIndex])
 
