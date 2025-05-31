01aa032537840ffb05f940a3811f999607f336a0
Иван Власенко <gimo135@yandex.ru>
2025-05-31 14:43:08 +0300

    Refactor gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,28 +1,25 @@
 import getRandomInRange from '../randomizer.js'
 
+const getGCD = (a, b) => {
+  if (b === 0) {
+    return String(a)
+  }
+
+  while (b != 0) {
+    const temp = a
+    a = b
+    b = temp % b
+  }
+  return String(a)
+}
+
 const game = () => {
   const firstNumber = getRandomInRange(1, 100)
   const secondNumber = getRandomInRange(1, 100)
-
+  const rules = 'Find the greatest common divisor of given numbers.'
   const question = `${firstNumber} ${secondNumber}`
 
-  const rules = 'Find the greatest common divisor of given numbers.'
-
-  const greatestDivisor = (() => {
-    let a = firstNumber
-    let b = secondNumber
-
-    if (b === 0) {
-      return String(a)
-    }
-
-    while (b != 0) {
-      const temp = a
-      a = b
-      b = temp % b
-    }
-    return String(a)
-  })()
+  const greatestDivisor = getGCD(firstNumber, secondNumber)
 
   return [question, greatestDivisor, rules]
 }
