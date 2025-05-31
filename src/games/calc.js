dde32be072ea9bfc8e29f85066b31f2428bbf067
Иван Власенко <gimo135@yandex.ru>
2025-05-31 14:14:00 +0300

    Refactor calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,25 +1,24 @@
 import getRandomInRange from '../randomizer.js'
 
+const getResult = (firstNumber, secondNumber, operator) => {
+  switch (operator) {
+    case '+':
+      return String(firstNumber + secondNumber)
+    case '-':
+      return String(firstNumber - secondNumber)
+    case '*':
+      return String(firstNumber * secondNumber)
+  }
+}
+
 const game = () => {
   const firstNumber = getRandomInRange(1, 100)
   const secondNumber = getRandomInRange(1, 100)
   const operators = ['+', '-', '*']
+  const operator = operators[getRandomInRange(0, 2)]
+  const expression = `${firstNumber} ${String(operator)} ${secondNumber}`
 
-  const [expression, correctAnswer] = (() => {
-    switch (operators[getRandomInRange(0, 2)]) {
-      case '+':
-        return [`${firstNumber} + ${secondNumber}`, String(firstNumber + secondNumber)]
-      case '-':
-        if (firstNumber > secondNumber) {
-          return [`${firstNumber} - ${secondNumber}`, String(firstNumber - secondNumber)]
-        }
-        else {
-          return [`${secondNumber} - ${firstNumber}`, String(secondNumber - firstNumber)]
-        }
-      case '*':
-        return [`${firstNumber} * ${secondNumber}`, String(firstNumber * secondNumber)]
-    }
-  })()
+  const correctAnswer = getResult(firstNumber, secondNumber, operator)
 
   console.log('What is the result of the expression?')
 
